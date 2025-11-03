e1832b7617b8210e0ce891cfee7e73c23bdf9415
yixianghuang2005 <alan20050921@gmail.com>
2025-11-03 16:31:27 +0800

    v5_地鼠隨機移動

--- app/src/main/java/tw/edu/pu/csim/tcyang/mole/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/mole/MainActivity.kt
@@ -21,6 +21,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
@@ -46,8 +48,18 @@ fun MoleScreen(moleViewModel: MoleViewModel = viewModel()) {
     //var counter by rememberSaveable { mutableLongStateOf(0) }
     val stay = moleViewModel.stay
 
+    // DP-to-pixel轉換
+    val density = LocalDensity.current
+
+    // 地鼠Dp轉Px
+    val moleSizeDp = 150.dp
+    val moleSizePx = with(density) { moleSizeDp.roundToPx() }
+
+
     Box (
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
+            .onSizeChanged { intSize ->  // 用來獲取全螢幕尺寸px
+               moleViewModel.getArea(intSize, moleSizePx) },
         Alignment.Center
     ) {
         Text("分數: $counter \n時間: $stay")
@@ -57,8 +69,8 @@ fun MoleScreen(moleViewModel: MoleViewModel = viewModel()) {
         painter = painterResource(id = R.drawable.mole),
         contentDescription = "地鼠",
         modifier = Modifier
-            .offset { IntOffset(600, 800) }
-            .size(150.dp)
+            .offset { IntOffset(moleViewModel.offsetX, moleViewModel.offsetY) }
+            .size(moleSizeDp)
             .clickable { moleViewModel.incrementCounter() }
     )
 }
--- app/src/main/java/tw/edu/pu/csim/tcyang/mole/MoleViewModel.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/mole/MoleViewModel.kt
@@ -2,7 +2,9 @@ package tw.edu.pu.csim.tcyang.mole
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.IntSize
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
@@ -15,6 +17,20 @@ class MoleViewModel : ViewModel() {
         private set  //表示只有 ViewModel 內部可以修改它
     var stay by mutableLongStateOf(0)
         private set // 設定為 private
+
+    var maxX by mutableStateOf(0)
+        private set
+
+    var maxY by mutableStateOf(0)
+        private set
+
+    var offsetX by mutableStateOf(0)
+        private set
+
+    var offsetY by mutableStateOf(0)
+        private set
+
+
 
 
     fun incrementCounter() {
@@ -29,11 +45,24 @@ class MoleViewModel : ViewModel() {
     private fun startCounting() {
         viewModelScope.launch {
             while (true) { // 無限循環，每秒增加一次
-                delay(1000L)
+                delay(500L)
                 stay++ // 計數器加 1，這會自動觸發 UI 更新
+                moveMole()
             }
         }
     }
+
+    fun getArea(gameSize: IntSize, moleSize:Int) {
+        maxX = gameSize.width - moleSize
+        maxY = gameSize.height - moleSize
+    }
+
+    fun moveMole() {
+        offsetX = (0..maxX).random()
+        offsetY = (0..maxY).random()
+    }
+
+
 
 
 
